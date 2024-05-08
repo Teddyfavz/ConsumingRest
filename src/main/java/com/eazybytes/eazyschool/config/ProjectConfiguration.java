9044e79d67efbf3bcc9fa248f710a31a13a4b44d
favz <Ogbonnafavz97@gmail.com>
2024-05-07 22:20:25 -0500

    Consuming Rest Services

--- src/main/java/com/eazybytes/eazyschool/config/ProjectConfiguration.java
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectConfiguration.java
@@ -1,8 +1,12 @@
 package com.eazybytes.eazyschool.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ProjectConfiguration {
@@ -10,5 +14,20 @@ public class ProjectConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
         return new BasicAuthRequestInterceptor("admin@favschool.com", "admin");
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.basicAuthentication("admin@favschool.com",
+                "admin").build();
+    }
+
+    @Bean
+    public WebClient webClient(){
+        return WebClient.builder()
+                .filter(ExchangeFilterFunctions.basicAuthentication("admin@favschool.com",
+                        "admin"))
+                .build();
     }
 }
